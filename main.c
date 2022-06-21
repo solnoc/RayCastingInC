f6f9f996d852d5337871dbd6c22826dcc7b91fc8
Solnoc Robert <solnocrobert@gmail.com>
2022-06-21 20:31:16 +0300

    Updated graphics encoding
    Put in enumerations for colors and block types inside the main function and the texture file. This cleans the code a lot and also makes it faster because we dont use strcmp for every pixel in the screen anymore...

--- main.c
+++ main.c
@@ -9,32 +9,8 @@
 #include <assert.h>
 #include <stdlib.h>
 
-#define TC_NRM  "\x1B[0m"
-#define TC_RED  "\x1B[1;31m"
-#define TC_GRN  "\x1B[1;32m"
-#define TC_YEL  "\x1B[1;33m"
-#define TC_BLU  "\x1B[1;34m"
-#define TC_MAG  "\x1B[1;35m"
-#define TC_CYN  "\x1B[1;36m"
-#define TC_WHT  "\x1B[1;37m"
-
-#define TC_B_NRM  "\x1B[0m"
-#define TC_B_RED  "\x1B[0;31m"
-#define TC_B_GRN  "\x1B[0;32m"
-#define TC_B_YEL  "\x1B[0;33m"
-#define TC_B_BLU  "\x1B[0;34m"
-#define TC_B_MAG  "\x1B[0;35m"
-#define TC_B_CYN  "\x1B[0;36m"
-#define TC_B_WHT  "\x1B[0;37m"
-
-#define TC_BG_NRM "\x1B[40m"
-#define TC_BG_RED "\x1B[41m"
-#define TC_BG_GRN "\x1B[42m"
-#define TC_BG_YEL "\x1B[43m"
-#define TC_BG_BLU "\x1B[44m"
-#define TC_BG_MAG "\x1B[45m"
-#define TC_BG_CYN "\x1B[46m"
-#define TC_BG_WHT "\x1B[47m"
+enum e_color{ NRM,RED,GRN,YEL,BLU,MAG,CYN,WHY };
+enum e_blockType{LIGHT, SHADED, DARK};
 
 int getch(void) {
       int c=0;
@@ -54,52 +30,68 @@ int getch(void) {
       assert(res==0);
       return(c);
 }
-int **buffer;
-char ***buffer_color;
-void draw_pixel(int i, int j, char color[], int block_type)
+
+enum e_blockType **buffer;
+enum e_color **buffer_color;
+void draw_pixel(int i, int j, enum e_color color, enum e_blockType block_type)
 {
-    char block[7];
-    bool draw = false;
-    switch(block_type)
+    if(buffer[i][j] != block_type || buffer_color[i][j] != color)
     {
-        case 0:
-            if(buffer[i][j] != 0 || strcmp(buffer_color[i][j],color))
-            {
-                strncpy(block, "\u2588", 5);
-                buffer[i][j] = 0;
-                strcpy(buffer_color[i][j], color);
-                draw = true;
-            }
+        buffer[i][j] = block_type;
+        buffer_color[i][j] = color;
+
+        char* drawColor = malloc(sizeof(char) * 13);
+        char* block = malloc(sizeof(char) * 7);
+        switch (color)
+        {
+        case NRM:
+            strcpy(drawColor, "\x1B[0m");
             break;
-        case 1:
-            if(buffer[i][j] != 1 || strcmp(buffer_color[i][j],color))
-            {
-                strncpy(block, "\u2593", 5);
-                buffer[i][j] = 1;
-                strcpy(buffer_color[i][j], color);
-                draw = true;
-            }
+        case RED:
+            strcpy(drawColor, "\x1B[1;31m");
             break;
-        case 2:
-            if(buffer[i][j] != 2 || strcmp(buffer_color[i][j],color))
-            {
-                strncpy(block, "\u2592", 5);
-                buffer[i][j] = 2;
-                strcpy(buffer_color[i][j], color);
-                draw = true;
-            }
+        case GRN:
+            strcpy(drawColor, "\x1B[1;32m");
+            break;
+        case YEL:
+            strcpy(drawColor, "\x1B[1;33m");
+            break;
+        case BLU:
+            strcpy(drawColor, "\x1B[1;34m");
+            break;
+        case MAG:
+            strcpy(drawColor, "\x1B[1;35m");
+            break;
+        case CYN:
+            strcpy(drawColor, "\x1B[1;36m");
+            break;
+        case WHY:
+            strcpy(drawColor, "\x1B[1;37m");
             break;
         default:
-            draw = true;
-            strncpy(block, "\u2588", 5);
+            strcpy(drawColor, "\x1B[1;37m");
             break;
-    }
-    if(draw)
-    {
+        }
+        switch (block_type)
+        {
+        case LIGHT:
+            strcpy(block, "\u2588");
+            break;
+        case SHADED:
+            strcpy(block, "\u2593");
+            break;
+        case DARK:
+            strcpy(block, "\u2592");
+            break;
+        default:
+            strcpy(block, "\u2592");
+            break;
+        }
+
         printf("\033[%d;%dH", i, j*2);
-        printf("%s%s",color,block);
+        printf("%s%s",drawColor,block);
         printf("\033[%d;%dH", i, j*2+1);
-        printf("%s%s",color,block);
+        printf("%s%s",drawColor,block);
     }
 }
 
@@ -139,29 +131,22 @@ void Initialize()
     height = ws.ws_row;
     width = ws.ws_col/2;
 
-    buffer = malloc(sizeof(int*) * height);
+    buffer = malloc(sizeof(enum e_blockType*) * height);
     for(int i=0;i<height;i++)
     {
-        buffer[i] = malloc(sizeof(int) * width);
+        buffer[i] = malloc(sizeof(enum e_blockType) * width);
     }
-    buffer_color = malloc(sizeof(char**) * height);
+    buffer_color = malloc(sizeof(enum e_color*) * height);
     for(int i=0;i<height;i++)
     {
-        buffer_color[i] = malloc(sizeof(char*) * width);
-    }
-    for(int i=0;i<height;i++)
-    {
-        for(int j=0;j<width;j++)
-        {
-            buffer_color[i][j] = malloc(sizeof(char) * 13);
-        }
+        buffer_color[i] = malloc(sizeof(enum e_color) * width);
     }
     for(int i=0;i<height;i++)
     {
         for(int j=0;j<width;j++)
         {
-            buffer[i][j] = -1;
-            strcpy(buffer_color[i][j],"TC_GRN");
+            buffer[i][j] = DARK;
+            buffer_color[i][j] = WHY;
         }
     }
     strncpy(map[0] , "####################", 20);
@@ -295,26 +280,26 @@ void UpdateAndDraw()
             {
                 if(j<celling)
                 {
-                    draw_pixel(j,i,TC_BLU,0);
+                    draw_pixel(j,i,BLU,LIGHT);
                 }
                 else if(j>= celling && j<floor)
                 {
                     if(d < 5.0f)
-                        draw_pixel(j,i,TC_YEL,0 + hitCorner);
+                        draw_pixel(j,i,YEL,LIGHT + hitCorner);
                     else if(d < 10.0f)
-                        draw_pixel(j,i,TC_YEL,1 + hitCorner);
+                        draw_pixel(j,i,YEL,SHADED + hitCorner);
                     else
-                        draw_pixel(j,i,TC_YEL,2);
+                        draw_pixel(j,i,YEL,DARK);
                 }
                 else
                 {
                     float poz = 1.0f - ((float)j - height/2.0f) / ((float)height/2.0f);
                     if(poz < 0.60f)
-                        draw_pixel(j,i,TC_GRN,0);
+                        draw_pixel(j,i,GRN,LIGHT);
                     else if(poz < 0.80f)
-                        draw_pixel(j,i,TC_GRN,1);
+                        draw_pixel(j,i,GRN,SHADED);
                     else
-                        draw_pixel(j,i,TC_GRN,2);
+                        draw_pixel(j,i,GRN,DARK);
                 }
             } 
         }
--- texture.c
+++ texture.c
@@ -5,90 +5,79 @@
 #include <string.h>
 #include <stdlib.h>
 
+enum e_color{ NRM,RED,GRN,YEL,BLU,MAG,CYN,WHY };
+enum e_blockType{LIGHT, SHADED, DARK};
 
-#define TC_NRM  "\x1B[0m"
-#define TC_RED  "\x1B[1;31m"
-#define TC_GRN  "\x1B[1;32m"
-#define TC_YEL  "\x1B[1;33m"
-#define TC_BLU  "\x1B[1;34m"
-#define TC_MAG  "\x1B[1;35m"
-#define TC_CYN  "\x1B[1;36m"
-#define TC_WHT  "\x1B[1;37m"
-
-#define TC_B_NRM  "\x1B[0m"
-#define TC_B_RED  "\x1B[0;31m"
-#define TC_B_GRN  "\x1B[0;32m"
-#define TC_B_YEL  "\x1B[0;33m"
-#define TC_B_BLU  "\x1B[0;34m"
-#define TC_B_MAG  "\x1B[0;35m"
-#define TC_B_CYN  "\x1B[0;36m"
-#define TC_B_WHT  "\x1B[0;37m"
-
-#define TC_BG_NRM "\x1B[40m"
-#define TC_BG_RED "\x1B[41m"
-#define TC_BG_GRN "\x1B[42m"
-#define TC_BG_YEL "\x1B[43m"
-#define TC_BG_BLU "\x1B[44m"
-#define TC_BG_MAG "\x1B[45m"
-#define TC_BG_CYN "\x1B[46m"
-#define TC_BG_WHT "\x1B[47m"
-
-
-int **buffer;
-char ***buffer_color;
-void draw_pixel(int i, int j, char* color, int block_type)
+enum e_blockType **buffer;
+enum e_color **buffer_color;
+void draw_pixel(int i, int j, enum e_color color, enum e_blockType block_type)
 {
-    char block[7];
-    bool draw = false;
-    switch(block_type)
+    if(buffer[i][j] != block_type || buffer_color[i][j] != color)
     {
-        case 0:
-            if(buffer[i][j] != 0 || strcmp(buffer_color[i][j],color))
-            {
-                strncpy(block, "\u2588", 5);
-                buffer[i][j] = 0;
-                strcpy(buffer_color[i][j], color);
-                draw = true;
-            }
+        buffer[i][j] = block_type;
+        buffer_color[i][j] = color;
+
+        char* drawColor = malloc(sizeof(char) * 13);
+        char* block = malloc(sizeof(char) * 7);
+        switch (color)
+        {
+        case NRM:
+            strcpy(drawColor, "\x1B[0m");
             break;
-        case 1:
-            if(buffer[i][j] != 1 || strcmp(buffer_color[i][j],color))
-            {
-                strncpy(block, "\u2593", 5);
-                buffer[i][j] = 1;
-                strcpy(buffer_color[i][j], color);
-                draw = true;
-            }
+        case RED:
+            strcpy(drawColor, "\x1B[1;31m");
             break;
-        case 2:
-            if(buffer[i][j] != 2 || strcmp(buffer_color[i][j],color))
-            {
-                strncpy(block, "\u2592", 5);
-                buffer[i][j] = 2;
-                strcpy(buffer_color[i][j], color);
-                draw = true;
-            }
+        case GRN:
+            strcpy(drawColor, "\x1B[1;32m");
+            break;
+        case YEL:
+            strcpy(drawColor, "\x1B[1;33m");
+            break;
+        case BLU:
+            strcpy(drawColor, "\x1B[1;34m");
+            break;
+        case MAG:
+            strcpy(drawColor, "\x1B[1;35m");
+            break;
+        case CYN:
+            strcpy(drawColor, "\x1B[1;36m");
+            break;
+        case WHY:
+            strcpy(drawColor, "\x1B[1;37m");
             break;
         default:
-            draw = true;
-            strncpy(block, "\u2588", 5);
+            strcpy(drawColor, "\x1B[1;37m");
             break;
-    }
-    if(draw)
-    {
+        }
+        switch (block_type)
+        {
+        case LIGHT:
+            strcpy(block, "\u2588");
+            break;
+        case SHADED:
+            strcpy(block, "\u2593");
+            break;
+        case DARK:
+            strcpy(block, "\u2592");
+            break;
+        default:
+            strcpy(block, "\u2592");
+            break;
+        }
+
         printf("\033[%d;%dH", i, j*2);
-        printf("%s%s",color,block);
+        printf("%s%s",drawColor,block);
         printf("\033[%d;%dH", i, j*2+1);
-        printf("%s%s",color,block);
+        printf("%s%s",drawColor,block);
     }
 }
-
 
 int height;
 int width;
 
 struct texture{
-    char ***encoding;
+    enum e_color** colorEncoding;
+    enum e_blockType** blockTypeEncoding;
     int height;
     int width;
 };
@@ -106,20 +95,19 @@ struct texture upload_texture(const char* filepath)
     token = strtok(NULL," ");
     textura.width = atoi(token);
 
-    textura.encoding = malloc(sizeof(char**) * textura.height);
+    textura.colorEncoding = malloc(sizeof(enum e_color*) * textura.height);
+    textura.blockTypeEncoding = malloc(sizeof(enum e_blockType*) * textura.height); 
     for(int i=0;i<textura.height;i++)
     {
-        textura.encoding[i] = malloc(sizeof(char*) * textura.width);
-        for(int j=0;j<width;j++)
-        {
-            textura.encoding[i][j] = malloc(sizeof(char) * 13);
-        }
+        textura.colorEncoding[i] = malloc(sizeof(enum e_color) * textura.width);
+        textura.blockTypeEncoding[i] = malloc(sizeof(enum e_blockType) * textura.width);
     }
     for(int i=0;i<textura.height;i++)
     {
         for(int j=0;j<textura.width;j++)
         {
-            fscanf(file, "%s",textura.encoding[i][j]);
+            fscanf(file, "%u",&textura.colorEncoding[i][j]);
+            fscanf(file, "%u",&textura.blockTypeEncoding[i][j]);
         }
     }
 
@@ -135,39 +123,32 @@ int main()
     height = ws.ws_row;
     width = ws.ws_col/2;
 
-    buffer = malloc(sizeof(int*) * height);
+    buffer = malloc(sizeof(enum e_blockType*) * height);
     for(int i=0;i<height;i++)
     {
-        buffer[i] = malloc(sizeof(int) * width);
+        buffer[i] = malloc(sizeof(enum e_blockType) * width);
     }
-    buffer_color = malloc(sizeof(char**) * height);
+    buffer_color = malloc(sizeof(enum e_color*) * height);
     for(int i=0;i<height;i++)
     {
-        buffer_color[i] = malloc(sizeof(char*) * width);
-    }
-    for(int i=0;i<height;i++)
-    {
-        for(int j=0;j<width;j++)
-        {
-            buffer_color[i][j] = malloc(sizeof(char) * 13);
-        }
+        buffer_color[i] = malloc(sizeof(enum e_color) * width);
     }
     for(int i=0;i<height;i++)
     {
         for(int j=0;j<width;j++)
         {
-            buffer[i][j] = -1;
-            strcpy(buffer_color[i][j],"TC_GRN");
+            buffer[i][j] = DARK;
+            buffer_color[i][j] = WHY;
         }
     }
-
-    struct texture wall = upload_texture("wall.tex");
+    
+    struct texture wall = upload_texture("wall.txt");
     
     for(int i=0;i<height;i++)
     {
         for(int j=0;j<width;j++)
         {
-            draw_pixel(i,j,wall.encoding[i % wall.height][j % wall.width],0);
+            draw_pixel(i,j,wall.colorEncoding[i % wall.height][j % wall.width],wall.blockTypeEncoding[i % wall.height][j % wall.width]);
         }
     }
 
